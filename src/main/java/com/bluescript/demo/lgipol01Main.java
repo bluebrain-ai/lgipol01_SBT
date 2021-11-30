23f4d589b9a14676bab8789bb8ceefdc09d2fa55
bluebrain-ai <bluebrainenterprise@gmail.com>
2021-11-30 14:03:55 +0530

    first commit

---
+++ src/main/java/com/bluescript/demo/lgipol01Main.java
@@ -0,0 +1,13 @@
+package com.bluescript.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lgipol01Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(lgipol01Main.class, args);
+    }
+
+}
